df10f7cf65d9b2124f94057fd9251f525d25bc0a
abbeykuyl <akuyl12@gmail.com>
2024-11-21 20:49:12 -0500

    commit.html

---
+++ Script.js
@@ -0,0 +1,62 @@
+var characters=['Harry', 'Hermione', 'Ron', 'Draco',
+    'Luna']
+    
+var FName = "Harry" 
+var House = "Gryffendor" 
+console.log("Welcome",FName, "of house", House,"!");
+
+var CharacterName = "Hermione"
+String; uppercaseName = CharacterName.toUpperCase("Hermione");
+String; uppercaseName = CharacterName.toLowerCase("Hermione");
+
+var Spell = "  Expelliarmus  "
+let str1 = " Expelliarmus ";
+console.log("\ntrim() example:", `"${str1.trim()}"`);
+
+var Quote = "I solemnly swear that I am up to no good"
+str = "I solemnly swear that I am up to no good"
+console.log("slice(11,16):", str.slice(11,16))
+
+var FirstName = " Ron "
+var LastName=" Weasley "
+console.log(FirstName+LastName)
+
+var Sentence = "Draco is a bad wizard"
+str= "Draco is a bad wizard"
+
+console.log("replacedstr:", str.replace("bad", "Good"))
+
+const house = ["Ravenclaw", "Hufflepuff","Gryffendor"];
+house.push("slytherin");
+console.log(house)
+
+const removehouse = house.pop();
+console.log(removehouse);
+
+const spells = ['Alohomora', 'Lumos', 'Nox'];
+let firstElement = spells.shift();
+console.log("After shift():", spells);
+console.log("Shifted element:", firstElement);
+
+const professors = ['Dumbledore', 'McGonagall', 'Snape', 'Hagrid']
+let slicedArr = professors.slice(2, 3);
+console.log("\nArray slice(1, 3):", slicedArr);
+
+const students = ['Neville', 'Seamus', 'Dean', 'Parvati']
+students.splice(1, 2,);
+console.log("After splice(1, 2,):", students);
+
+var phrase=' Mischief Managed ';
+let str2 = ' Mischief Managed ';
+toLowerCase(phrase)
+console.log("\ntrim() example:", `"${str1.trim()}"`);
+console.log(Phrase, ",", "Harry.");
+
+const wizards = ['Harry', 'Hermione', 'Ron'];
+wizards.push("Luna", "Draco")
+console.log(wizards)
+
+const message = 'Welcome to Hogwarts School of Witchcraft'
+let slicedwizards=wizards.slice(3)
+console.log(message,"castle")
+
